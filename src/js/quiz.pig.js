e9a0838986d9ab8cb9bebd7efdf79249ed1f4fd1
XiaoS342 <158113683+XiaoS342@users.noreply.github.com>
2024-04-30 15:27:57 -0400

    1

---
+++ src/js/quiz.pig.js
@@ -0,0 +1,34 @@
+// This is where you should write all JavaScript
+// for your project. Remember a few things as you start!
+// - Use let or const for all variables
+// - Do not use jQuery - use JavaScript instead
+// - Do not use onclick - use addEventListener instead
+// - Run npm run test regularly to check autograding
+// - You'll need to link this file to your HTML :)
+// This is where you should write all JavaScript
+// for your project. Remember a few things as you start!
+// - Use let or const for all variables
+// - Do not use jQuery - use JavaScript instead
+// - Do not use onclick - use addEventListener instead
+// - Run npm run test regularly to check autograding
+// - You'll need to link this file to your HTML :)
+
+
+// quiz.js 文件内容
+document.addEventListener('DOMContentLoaded', () => {
+    const answerA = document.getElementById('answer-a');
+    const answerB = document.getElementById('answer-b');
+    const answerC = document.getElementById('answer-c');
+  
+    answerA.addEventListener('click', () => {
+      window.location.href = 'pig-A.html'; // A 选项的结果页面
+    });
+  
+    answerB.addEventListener('click', () => {
+      window.location.href = 'pig-B.html'; // B 选项的结果页面
+    });
+  
+    answerC.addEventListener('click', () => {
+      window.location.href = 'pig-C.html'; // C 选项的结果页面
+    });
+  });
